a59766473c247932792783c481b6f14729a91910
R1cecracker <troy.harcoan0@gmail.com>
2025-05-19 04:40:29 +0000

    fixes and the code works

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Person():
         return
     
     def printFullName(self):
-        return
+        print(f"{self.fname} {self.lname}")
 
 class Student(Person):
     def __init__ (self, fname, lname, studID, hg):
@@ -14,15 +14,39 @@ class Student(Person):
         self.hg = hg
         self.subject = []
         return
+    
+    def enrolClass(self, subjectName, studID):
+        s1 = Subject(subjectName, studID)
+        self.subject.append(s1)
 
-    def enrolClass(self):
-        s1 = subject()
-        s1.subjectName = subjectName
-        s1.StudentID = studID
-        subjects.append(s1)
+    def ShowClasses(self):
+        for i in range(len(self.subject)):
+            print("Student ID: ", self.subject[i].studentID)
+            print("Subject Name: ", self.subject[i].subjectName)
+            print()
+        return
 
-    def showClasses(self):
-        for sub in subjects:
+class Parent(Person):
+    def __init__(self, fname, lname, occupation, alumni):
+        super().__init__(fname, lname)
+        self.occupation = occupation
+        self.alumni = alumni
 
 class Subject():
-    def
+    def __init__ (self, subjectName, studentID):
+        self.subjectName = subjectName
+        self.studentID = studentID
+
+    def printStudentList(self):
+        print(f"Student ID: {self.studentID}, Subject: {self.subjectName}")
+
+#main
+s1 = Student("Troy", "Harcoan", 12345, "heber")
+s1.printFullName()
+s1.enrolClass("Math Advanced", 12345)
+s1.enrolClass("English Advanced", 12345)
+s1.enrolClass("Physics", 12345)
+s1.enrolClass("Ancient History", 12345)
+s1.enrolClass("Music", 12345)
+s1.enrolClass("Software Engineering", 12345)
+s1.ShowClasses()
